121598c7f9034c032e4908059f3cc358e5cebb5c
Ciro Mello <ciromelloebsco@gmail.com>
2019-09-25 13:48:58 -0400

    Update test13_2.py

--- test13_2.py
+++ test13_2.py
@@ -1,45 +1,103 @@
 exercise 13_2 
 
-# 13.2 - Manipulate PDF Files
-# Solutions to review exercises
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 24 15:38:02 2019
+
+@author: cmello
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 13 17:22:50 2019
+
+@author: cmello
+"""
 
 import os
-import copy
+
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
-
-# Exercise 1
 path = "C:/Users/cmello/Documents/" \
 "Python Basics Book Dan Bader/" \
 "exercises_chapter_13/"
 
-
-input_file_path = os.path.join(path, "Walrus.pdf")
-input_file = PdfFileReader(input_file_path)
-output_PDF = PdfFileWriter()
-
-input_file.decrypt("IamtheWalrus")  # decrypt the input file
+input_file_path = os.path.join(path, "output/Walrus.pdf")
+input_pdf = PdfFileReader(input_file_path)
+output_pdf = PdfFileWriter()
 
 
-# Exercise 2
-for page_num in range(0, input_file.getNumPages()):
-    # rotate pages (call everything page_left for now; will make a copy)
-    page_left = input_file.getPage(page_num)
-    page_left.rotateCounterClockwise(90)
+# Decrypt the PDF file
+input_pdf.decrypt("IamtheWalrus")  # without this, I get an error message.
 
-    page_right = copy.copy(page_left)  # split each page in half
-    upper_right = page_left.mediaBox.upperRight  # get original page corner
+# 2
+num_pages = input_pdf.getNumPages()
+for n in range(0, num_pages):
+	page = input_pdf.getPage(n)
+	page.rotateClockwise(270)
+	output_pdf.addPage(page)
 
-    # crop and add left-side page
-    page_left.mediaBox.upperRight = (upper_right[0] / 2, upper_right[1])
-    output_PDF.addPage(page_left)
-    # crop and add right-side page
-    page_right.mediaBox.upperLeft = (upper_right[0] / 2, upper_right[1])
-    output_PDF.addPage(page_right)
+output_file_path = os.path.join(path, "output/Walrus Rotated2.pdf")
 
-
-# Exercise 3
-# save new pages to an output file
-output_file_path = os.path.join(path, "Output/Updated Walrus.pdf")
 with open(output_file_path, "wb") as output_file:
-    output_PDF.write(output_file)
+	output_pdf.write(output_file)
+
+
+
+"""
+CIRO START FROM STEP # 3
+import copy
+
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+import os
+
+path = "C:/Users/cmello/Documents/" \
+"Python Basics Book Dan Bader/" \
+"exercises_chapter_13/output/Walrus Rotated.pdf"
+
+input_pdf = PdfFileReader(path)
+
+input_pdf.decrypt("IamtheWalrus")
+
+page.mediaBox
+
+"""
+
+
+# 3 
+
+path2 = "C:/Users/cmello/Documents/" \
+"Python Basics Book Dan Bader/" \
+"exercises_chapter_13/Output/"
+
+input_file_path2 = os.path.join(path2, "Walrus Rotated2.pdf")
+input2_pdf = PdfFileReader(input_file_path2)
+output2_pdf = PdfFileWriter()
+
+
+#input_file_path2.decrypt("IamtheWalrus")
+#input2_pdf.decrypt("IamtheWalrus")
+
+
+for page_num in range(0, input2_pdf.getNumPages()):
+	page_left = input2_pdf.getPage(page_num)
+	page_right = copy.copy(page_left)
+	# Calculate the new coordinates for the upper-right
+	# corner of the page_left and the upper-left
+	# corner of page_right
+	upper_right = page_left.mediaBox.upperRight
+	new_coords = (upper_right[0]/2, upper_right[1])
+	# Crop and add left-side page to the ouput file
+	page_left.mediaBox.upperRight = new_coords
+	output2_pdf.addPage(page_left)
+	# Crop and add right-side page to the output file
+	page_right.mediaBox.upperLeft = new_coords
+	output2_pdf.addPage(page_right)
+
+# 4
+
+output_file_path2 = os.path.join(path2, "Walrus_exercise13_.pdf")
+
+with open(output_file_path2, "wb") as output_file:
+	output2_pdf.write(output_file)
